5ae4f36668102c34522bf60ae793194ccf0f39d6
tishkatishka1-dotcom <tishka.tishka1@gmail.com>
2026-01-29 15:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 app.get('/', (req, res) => {
   res.send('Flirt chat platform is running 🚀');
